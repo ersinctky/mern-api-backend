13c54cf30119284a959e6c69d7dcc968c32a2f03
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-02-11 14:53:16 +0300

    first route

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.get("/api/home", (req, res) => {
+  res.status(200).json({ message: "Home" });
+});
+
 app.listen(port, () => console.log(`Server started at port ${port}`));
