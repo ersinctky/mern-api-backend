5f94fc7b3905cf3d40eeb38148517fbc5583d1cd
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-02-11 16:53:57 +0300

    server.js updated (accepting body data)

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 
